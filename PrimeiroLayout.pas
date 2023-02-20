49ef4d4d133bf0edfa861cb1cbd0208e8145f7c6
Yan Shinayder <shinayderepcar@outlook.com>
2023-02-20 17:23:26 -0300

    Primeiro Layout Iniciado

--- PrimeiroLayout.pas
+++ PrimeiroLayout.pas
@@ -4,10 +4,14 @@ interface
 
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
-  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs;
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Ani,
+  FMX.Objects, FMX.Controls.Presentation, FMX.StdCtrls;
 
 type
   TForm1 = class(TForm)
+    Base1: TRectangle;
+    Image1: TImage;
+    Label1: TLabel;
   private
     { Private declarations }
   public
@@ -20,5 +24,10 @@ var
 implementation
 
 {$R *.fmx}
+{$R *.Moto360.fmx ANDROID}
+{$R *.XLgXhdpiTb.fmx ANDROID}
+{$R *.SSW3.fmx ANDROID}
+{$R *.NmXhdpiPh.fmx ANDROID}
+{$R *.LgXhdpiPh.fmx ANDROID}
 
 end.
